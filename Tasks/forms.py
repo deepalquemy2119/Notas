119a57476660a9cee7b7edbac6c4a0bc8f617b2c
deepalquemy2119 <deepalquemy2119@gmail.com>
2025-02-09 21:41:38 -0300

    mejorar la UI de create_task

--- Tasks/forms.py
+++ Tasks/forms.py
@@ -1,7 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        labels = {
+            'title': 'Task Title',
+            'description': 'Task Description',
+            'important': 'Mark as Important',
+        }
+        error_messages = {
+            'title': {'required': 'Please enter a task title.'},
+            'description': {'required': 'Please enter a task description.'},
+        }
