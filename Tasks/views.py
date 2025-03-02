69279b9191ce31802c54b89fb6a6ca129af84df2
deepalquemy2119 <deepalquemy2119@gmail.com>
2025-03-01 23:00:59 -0300

    comm 2.1.1.2

--- Tasks/views.py
+++ Tasks/views.py
@@ -21,36 +21,13 @@ def signup(request):
         if form.is_valid():
             form.save()
             return redirect('login')
-        # return render(request, 'signup.html', {"form": UserCreationForm})
+        
     else:
 
-        if request.POST["password1"] == request.POST["password2"]:
-            try:
-                user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"])
-                user.save()
-                login(request, user)
-                return redirect('tasks')
-            except IntegrityError:
-                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
+        error: "error"
 
-        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
+    return render(request, 'signup.html')
 
-
-
-""" from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # O redirige a una página de éxito
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
- """
 
 
 
